29cc59aa82576af6d32b45640e6d8736ea44b4e6
Ricardo Ramos <rcdo.ramos@gmail.com>
2021-10-06 00:06:47 -0300

    Fix CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FakeArtist.MixProject do
     [
       app: :fake_artist,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
